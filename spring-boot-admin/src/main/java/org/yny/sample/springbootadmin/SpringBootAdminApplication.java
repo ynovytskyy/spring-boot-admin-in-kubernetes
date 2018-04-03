947fe049dc09f7b9e952489d9f6b0dcb9510b7c0
Yaroslav Novytskyy <ynovytskyy@gmail.com>
2018-04-03 15:35:44 -0400

    - finished spring boot admin;

--- spring-boot-admin/src/main/java/org/yny/sample/springbootadmin/SpringBootAdminApplication.java
+++ spring-boot-admin/src/main/java/org/yny/sample/springbootadmin/SpringBootAdminApplication.java
@@ -1,9 +1,13 @@
 package org.yny.sample.springbootadmin;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
 public class SpringBootAdminApplication {
 
 	public static void main(String[] args) {
